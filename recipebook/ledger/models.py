4495d5037afa69520f6c39cf93ed4daed8569687
fwanana <francine.denise.lee@student.ateneo.edu>
2026-02-19 08:38:14 +0800

    Updated models.py
    added ingredient, recipe, and recipeingredient models.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,14 @@
+from datetime import datetime
 from django.db import models
+#from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=3)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
