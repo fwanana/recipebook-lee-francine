62d96a55847ec7847c0f3fa6a8788257dee5af8b
fwanana <francine.denise.lee@student.ateneo.edu>
2026-03-12 19:10:45 +0800

    created new page for creating new recipes

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from .models import Recipe
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import RecipeForm
 
 
 class RecipeListView(ListView):
@@ -15,3 +17,9 @@ class RecipeListView(ListView):
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'recipe_form.html'
